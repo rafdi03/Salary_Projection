610b909bd577a2a0248ea29ce10690264859fff6
rafdi -5 <141027900+rafdi03@users.noreply.github.com>
2023-09-22 18:29:10 +0700

    Update import pandas as pd.py

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -2,10 +2,8 @@ import pandas as pd
 import locale
 import matplotlib.pyplot as plt
 
-# Atur locale sesuai dengan preferensi Indonesia
 locale.setlocale(locale.LC_ALL, 'id_ID')
 
-# Load data dan latih model seperti yang sudah Anda lakukan sebelumnya
 df = pd.read_csv("Salary_Data.csv")
 X = df.iloc[:, :-1]
 y = df.iloc[:, 1]
@@ -17,22 +15,14 @@ from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Plot data gaji
 plt.scatter(X_train, y_train, color="green")
 plt.plot(X_train, regressor.predict(X_train), color="red")
 plt.title("Tahun Pengalaman VS Gaji")
 plt.xlabel("Tahun Pengalaman")
-plt.ylabel("Gaji (dalam Rupiah)")  # Label Y diubah menjadi "Gaji (dalam Rupiah)"
+plt.ylabel("Gaji") 
 plt.show()
 
-# Mintalah input dari pengguna untuk tahun pengalaman
-years_of_experience = float(input("Masukkan jumlah tahun pengalaman: "))  # Gunakan float untuk memungkinkan input desimal jika perlu
-
-# Prediksi gaji berdasarkan input pengguna
+years_of_experience = float(input("Masukkan jumlah tahun pengalaman: "))
 predicted_salary = regressor.predict([[years_of_experience]])
-
-# Format nilai predicted_salary menjadi format mata uang Rupiah
 formatted_salary = locale.currency(predicted_salary[0], grouping=True)
-
-# Cetak output yang mencakup tahun pengalaman dan gaji yang cocok dalam format Rupiah
 print(f"Untuk seseorang dengan {years_of_experience} tahun pengalaman, gaji yang diprediksi adalah: {formatted_salary}")
